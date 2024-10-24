f22a4aaa1dbd517fbd6afd24a5312cb014759be6
Polarthebear <thatotae325@gmail.com>
2024-10-24 20:23:52 +0000

    Task 7.

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,49 +1,26 @@
-class Airport {
-  // Constructor
+export default class Airport {
   constructor(name, code) {
-    // Validating input types
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    if (typeof code !== 'string') {
-      throw new TypeError('Code must be a string');
-    }
-
-    // Assigning to underscore properties
-    this._name = name;
-    this._code = code;
+    this.name = name;
+    this.code = code;
   }
 
-  // Getter for name
   get name() {
     return this._name;
   }
 
-  // Setter for name
   set name(value) {
-    if (typeof value !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
     this._name = value;
   }
 
-  // Getter for code
   get code() {
     return this._code;
   }
 
-  // Setter for code
   set code(value) {
-    if (typeof value !== 'string') {
-      throw new TypeError('Code must be a string');
-    }
     this._code = value;
   }
 
-  // Custom string description of the class (returns the airport code)
-  toString() {
+  get [Symbol.toStringTag]() {
     return this._code;
   }
 }
-
-export default Airport;
