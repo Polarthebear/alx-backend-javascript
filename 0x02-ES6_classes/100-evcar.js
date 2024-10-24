d74403d0609128f57c9b37c3f6cca65714b0f186
Polarthebear <thatotae325@gmail.com>
2024-10-24 21:51:19 +0000

    Advanced Task.

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,22 +1,20 @@
+// Import the Car class from the 10-car.js file
 import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
+    // Call the parent class constructor with brand, motor, and color
     super(brand, motor, color);
-    this.range = range;
+    this._range = range;
   }
 
+  // Getter for range
   get range() {
     return this._range;
   }
 
-  set range(value) {
-    this._range = value;
-  }
-
+  // Override cloneCar method to return an instance of the parent Car class
   cloneCar() {
-    const Species = super.constructor[Symbol.species];
-
-    return new Species();
+    return new Car(this._brand, this._motor, this._color);
   }
 }
